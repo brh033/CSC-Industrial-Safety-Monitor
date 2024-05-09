5546470917cd499f01074cfe272aa1f09e6959c2
unknown <brh033@latech.edu>
2024-05-08 21:15:54 -0500

    Make MQ2 gauge refresh

--- GUItestSerial.py
+++ GUItestSerial.py
@@ -6,20 +6,15 @@ import serial
 ser = serial.Serial("/dev/ttyACM0", 9600, timeout=5)
 ser.reset_input_buffer()
 
-while True:
-    if ser.in_waiting > 0:
-        line = ser.readline().decode('utf-8').rstrip()
-        print(line)
-
 root = tk.Tk()
 
-p1 = tk_tools.RotaryScale(root, max_value=2500.0, unit='cpm')
+p1 = tk_tools.RotaryScale(root, max_value=2500.0, unit='ppm')
 p1.grid(row=1,column=0,sticky=N+S+E+W)
 
-p2 = tk_tools.RotaryScale(root, max_value=2500.0, unit='cpm')
+p2 = tk_tools.RotaryScale(root, max_value=2500.0, unit='ppm')
 p2.grid(row=1,column=1,sticky=N+S+E+W)
 
-p3 = tk_tools.RotaryScale(root, max_value=2500.0, unit='cpm')
+p3 = tk_tools.RotaryScale(root, max_value=2500.0, unit='ppm')
 p3.grid(row=2,column=0,sticky=N+S+E+W)
 
 p4 = tk_tools.RotaryScale(root, max_value=2500.0, unit='cpm')
@@ -30,10 +25,22 @@ p1.set_value(count)
 
 def update_gauge():
     global p1, count
-    count += 1 #geiger_input, mq-2_input, mq-8_input, mq-9_input
-    p1.set_value(count)
-    root.after(50, update_gauge)
+    #count += 1 #geiger_input, mq-2_input, mq-8_input, mq-9_input
+    p1.set_value(dict.get(MQ2))
+    #root.after(50, update_gauge)
 
-root.after(50, update_gauge)
+#root.after(50, update_gauge)
 root.title("Guardian Sense-All")
 root.mainloop()
+dict = {
+    "MQ2": 0,
+    "MQ8": 0,
+    "MQ9": 0,
+    "rad": 0
+}
+while True:
+    if ser.in_waiting > 0:
+        line = ser.readline().decode('utf-8').rstrip()
+        print(line)
+        dict.update(MQ2 = line[5:8])
+        update_gauge()
