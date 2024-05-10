07a7afa7a8fd7109b3e01ac6f39bbdafc75eca3f
unknown <brh033@latech.edu>
2024-05-09 20:23:29 -0500

    Final working GUI with serial integration

--- GUItestSerial.py
+++ GUItestSerial.py
@@ -4,34 +4,24 @@ import tk_tools
 import serial
 
 ser = serial.Serial("/dev/ttyACM0", 9600, timeout=5)
-ser.reset_input_buffer()
+#ser.reset_input_buffer()
 
 root = tk.Tk()
+root.title("Guardian Sense-All")
+root.geometry("800x600")
 
-p1 = tk_tools.RotaryScale(root, max_value=2500.0, unit='ppm')
+p1 = tk_tools.Gauge(root, width=400, height=300, max_value=600, label='MQ2', unit='ppm')
 p1.grid(row=1,column=0,sticky=N+S+E+W)
 
-p2 = tk_tools.RotaryScale(root, max_value=2500.0, unit='ppm')
+p2 = tk_tools.Gauge(root, width=400, height=300, max_value=600, label='MQ8', unit='ppm')
 p2.grid(row=1,column=1,sticky=N+S+E+W)
 
-p3 = tk_tools.RotaryScale(root, max_value=2500.0, unit='ppm')
+p3 = tk_tools.Gauge(root, width=400, height=300, max_value=600, label='MQ9', unit='ppm')
 p3.grid(row=2,column=0,sticky=N+S+E+W)
 
-p4 = tk_tools.RotaryScale(root, max_value=2500.0, unit='cpm')
+p4 = tk_tools.Gauge(root, width=400, height=300, max_value=600, label='rad', unit='cpm')
 p4.grid(row=2,column=1,sticky=N+S+E+W)
 
-count = 30.0
-p1.set_value(count)
-
-def update_gauge():
-    global p1, count
-    #count += 1 #geiger_input, mq-2_input, mq-8_input, mq-9_input
-    p1.set_value(dict.get(MQ2))
-    #root.after(50, update_gauge)
-
-#root.after(50, update_gauge)
-root.title("Guardian Sense-All")
-root.mainloop()
 dict = {
     "MQ2": 0,
     "MQ8": 0,
@@ -40,7 +30,17 @@ dict = {
 }
 while True:
     if ser.in_waiting > 0:
-        line = ser.readline().decode('utf-8').rstrip()
-        print(line)
-        dict.update(MQ2 = line[5:8])
-        update_gauge()
+        for i in range(0,2):
+          line = ser.readline().decode('utf-8').rstrip()
+          dict.update(MQ2 = line[5:8])
+          dict.update(MQ8 = line[14:17])
+          dict.update(MQ9 = line[23:26])
+          dict.update(rad = line[44:47])
+          i += 1
+        p1.set_value(int(dict.get("MQ2")))
+        p2.set_value(int(dict.get("MQ8")))
+        p3.set_value(int(dict.get("MQ9")))
+        p4.set_value(int(dict.get("rad")))
+        root.update_idletasks()
+        root.update()
+
